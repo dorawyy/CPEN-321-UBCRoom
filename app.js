aba7d49aa1e29ceca35fe53bbc1d7f6f49cc35f7
Robin Lai <54918500+Robin-Lai-YY@users.noreply.github.com>
2020-11-08 20:06:58 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 const { exec } = require("child_process");
+const { ObjectId } = require("mongodb");
 var app = express();
 app.use(express.json());
 var db;
@@ -8,11 +9,13 @@ var s_price, s_location, s_types;
 const port1 = process.env.PORT || 3001;
 
 var MongoClient = require("mongodb").MongoClient;
+const ID = require("mongodb").ObjectId
 
 MongoClient.connect("mongodb://localhost:27017",
     { useUnifiedTopology: true },
     (err, client) =>{
         db = client.db("list");
+        db_logic = client.db("logic");
     }
 );
 
@@ -63,55 +66,173 @@ app.get('/search', function(req, res){
                     console.log(err);
                     res.send(err);
                 } else {
-                    var dataf = [];
-                    for(let i = 0; i < 4; i++){
-                        if(i >= data.length) break;
-                        dataf.push(data[data.length - i - 1]);
-
-                    }
-                    console.log(dataf);
-                    res.send(dataf);
+                    var dataf = [];
+                    for(let i = 0; i < 4; i++){
+                        if(i >= data.length) break;
+                        dataf.push(data[data.length - i - 1]);
+                    }
+                    data.forEach(elem => {
+                        elem.old_id = String(elem._id);
+                        delete elem._id;
+                    });        
+                    db_logic.collection("logic").insertMany(data,
+                        (err, request) =>{
+                            if(err) return console.log(err);
+                    });
+                    //console.log(dataf);
+                    res.send(dataf);
                 }
             });
 });
 
+var mylogic = async function(re, ids, index, cd) {
+    db_logic.collection('logic').find({'old_id':{$nin:ids}}).toArray(function(err, data){
+        if(err){
+            res.send(err);
+        }
+        else if(data.length > 0){
+            var pick = data[Math.floor(Math.random() * data.length)];
+            ids.push(pick.old_id);
+            pick._id = ObjectId(pick.old_id);
+            delete pick.old_id;
+            re.push(pick);
+            if(index == 4) cd();
+            else{
+                console.log(ids);
+                mylogic(re,ids,index+1,cd);
+            }
+        }
+        else{
+            if(index == 4) cd();
+            else{
+                console.log(ids);
+                mylogic(re,ids,index+1,cd);
+            }
+        }
+    });
+}
+
+
+
+
+app.get('/logic', function(req,res){
+    var id1,id2,id3;
+    var re = [];
+    var ids = [];
+    mylogic(re,ids,1,()=>{console.log(re); res.send(re);});
+
+    // db_logic.collection('logic').find().toArray(function(err, data){
+    //     if(err){
+    //         res.send(err);
+    //     }
+    //     else if(data.length > 0){
+    //         console.log(data);
+    //         var pick = data[Math.floor(Math.random() * data.length)];
+    //         id1 = pick.old_id;
+    //         console.log(id1);
+    //         pick._id = ObjectId(pick.old_id);
+    //         delete pick.old_id;
+    //         re.push(pick);
+    //     }
+    // });
+    // db_logic.collection('logic').find({"old_id":{$not: { $eq: id1}}}).toArray(function(err, data){
+    //     console.log("111111111111111111111111");
+    //     if({"old_id":{$not: { $eq: id1}}}) console.log("true");
+        
+    //     console.log("111111111111111111111111");
+    //     if(err){
+    //         res.send(err);
+    //     }
+    //     else if(data.length > 0){
+    //         console.log(data);
+    //         var pick = data[Math.floor(Math.random() * data.length)];
+    //         id2 = pick.old_id;
+    //         if (pick.old_id === id1) console.log("?????????????????????");
+    //         else{
+    //             console.log(pick.old_id);
+    //             console.log(id1);
+                
+    //             console.log(typeof(pick.old_id));
+    //             console.log(typeof(id1));
+    //         }
+    //         pick._id = ObjectId(pick.old_id);
+    //         delete pick.old_id;
+    //         re.push(pick);
+    //     }
+    // });
+    // db_logic.collection('logic').find({"old_id":{$nin:[id1,id2]}}).toArray(function(err, data){
+    //     if(err){
+    //         res.send(err);
+    //     }
+    //     else if(data.length > 0){
+    //         console.log(data);
+    //         var pick = data[Math.floor(Math.random() * data.length)];
+    //         id3 = pick.old_id;
+    //         pick._id = ObjectId(pick.old_id);
+    //         delete pick.old_id;
+    //         re.push(pick);
+    //     }
+    // });
+    // db_logic.collection('logic').find({"old_id":{$nin:[id1,id2,id3]}}).toArray(function(err, data){
+    //     if(err){
+    //         res.send(err);
+    //     }
+    //     else if(data.length > 0){
+    //         console.log(data);
+    //         var pick = data[Math.floor(Math.random() * data.length)];
+    //         pick._id = ObjectId(pick.old_id);
+    //         delete pick.old_id;
+    //         re.push(pick);
+    //         console.log(re);
+    //         res.send(re);
+    //     }
+    //     else{
+    //         console.log(re);
+    //         res.send(re);
+    //     }
+    // });
+
+    
+});
+
+
 // test search with mockmysearch()
 
-var mockmysearch = jest.fn();
+// var mockmysearch = jest.fn();
 
-mockmysearch
-    .mockReturnValueOnce([{
-        price: 3000,
-        location: "PV",
-        types: "1",
-        phone: "1",
-        email: "1",
-        descript: "1"
-        }]
+// mockmysearch
+//     .mockReturnValueOnce([{
+//         price: 3000,
+//         location: "PV",
+//         types: "1",
+//         phone: "1",
+//         email: "1",
+//         descript: "1"
+//         }]
         
-    )
-    .mockReturnValue([{
-        price: 3000,
-        location: "PV",
-        types: "1",
-        phone: "1",
-        email: "1",
-        descript: "1"
-        },
-        {
-        price: 3000,
-        location: "PV",
-        types: "1",
-        phone: "1",
-        email: "1",
-        descript: "1"
-        }])
+//     )
+//     .mockReturnValue([{
+//         price: 3000,
+//         location: "PV",
+//         types: "1",
+//         phone: "1",
+//         email: "1",
+//         descript: "1"
+//         },
+//         {
+//         price: 3000,
+//         location: "PV",
+//         types: "1",
+//         phone: "1",
+//         email: "1",
+//         descript: "1"
+//         }])
 
 
 
-app.get('/testsearch', function(req, res){
-    res.status(200).json(mockmysearch());
-});
+// app.get('/testsearch', function(req, res){
+//     res.status(200).json(mockmysearch());
+// });
 
 
 
